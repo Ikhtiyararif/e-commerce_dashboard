9d5c65065342ae350602638be56622275e625848
Ikhtiyararif <ikhtiyararif@gmail.com>
2025-03-21 17:29:32 +0700

    Manambahkan filtering data

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,9 +11,27 @@ except FileNotFoundError:
     st.error("File main_dataset.csv tidak ditemukan di direktori dashboard/")
     st.stop()
 
+# Konversi kolom ke datetime
+main_df['order_purchase_timestamp'] = pd.to_datetime(main_df['order_purchase_timestamp'])
+
 #Sidebar
 with st.sidebar:
     st.image("dashboard/logo ecom.png")
+    st.header("Filter Rentang Waktu")
+    start_date = st.date_input("Tanggal Mulai", main_df['order_purchase_timestamp'].min().date())
+    end_date = st.date_input("Tanggal Akhir", main_df['order_purchase_timestamp'].max().date())
+    kategori_produk = main_df['product_category_name'].unique().tolist()
+    kategori_produk.insert(0, "Semua") # Menambahkan opsi "Semua"
+    pilih_kategori = st.selectbox("Pilih Kategori Produk", kategori_produk)
+
+#Filter data berdasarkan rentang waktu
+filtered_df = main_df[(main_df['order_purchase_timestamp'].dt.date >= start_date) & (main_df['order_purchase_timestamp'].dt.date <= end_date)]
+
+# Filter data berdasarkan rentang waktu dan kategori
+if pilih_kategori == "Semua":
+    filtered_df = main_df[(main_df['order_purchase_timestamp'].dt.date >= start_date) & (main_df['order_purchase_timestamp'].dt.date <= end_date)]
+else:
+    filtered_df = main_df[(main_df['order_purchase_timestamp'].dt.date >= start_date) & (main_df['order_purchase_timestamp'].dt.date <= end_date) & (main_df['product_category_name'] == pilih_kategori)]
 
 #Judul Dashboard
 st.header("Dashboard E-Commerce")
@@ -21,7 +39,7 @@ st.header("Dashboard E-Commerce")
 #Statistik Skor Review
 st.subheader("Statistik Skor Review")
 
-byreview_df = main_df.groupby(by="review_score")["review_id"].nunique().reset_index()
+byreview_df = filtered_df.groupby(by="review_score")["review_id"].nunique().reset_index()
 byreview_df.rename(columns={"review_id": "review_count"}, inplace=True)
 
 fig_review_stats, ax_review_stats = plt.subplots(figsize=(12, 8))
@@ -44,7 +62,7 @@ st.pyplot(fig_review_stats)
 #Skor review terbanyak
 st.subheader("Skor Review dengan Jumlah Nilai Paling Banyak")
 
-counts = main_df['review_score'].value_counts()
+counts = filtered_df['review_score'].value_counts()
 sorted_index = sorted(counts.index)
 
 fig_highest_review, ax_max_review = plt.subplots(figsize=(10, 6))
@@ -64,7 +82,7 @@ st.pyplot(fig_highest_review)
 # Visualisasi Skor Review dengan Jumlah Nilai Paling Sedikit
 st.subheader("Skor Review dengan Jumlah Nilai Paling Sedikit")
 
-counts = main_df['review_score'].value_counts()
+counts = filtered_df['review_score'].value_counts()
 sorted_index = sorted(counts.index)
 
 fig_lowest_review, ax_lowest_review = plt.subplots(figsize=(12, 6))
@@ -83,11 +101,9 @@ st.pyplot(fig_lowest_review)
 
 #Tren Penjualan Tahun ke Tahun
 #Visualisasi data order_purchase_timestamp
-main_df['order_purchase_timestamp'] = pd.to_datetime(main_df['order_purchase_timestamp'])
+filtered_df['year'] = filtered_df['order_purchase_timestamp'].dt.year
 
-main_df['year'] = main_df['order_purchase_timestamp'].dt.year
-
-sales_by_year = main_df['year'].value_counts().sort_index()
+sales_by_year = filtered_df['year'].value_counts().sort_index()
 
 st.subheader("Tren Penjualan dari Tahun ke Tahun")
 
@@ -98,9 +114,8 @@ ax.set_xticks(sales_by_year.index)
 
 st.pyplot(fig)
 
-
 #Karena memang kategorinya terlalu banyak, hanya akan mengambil 5 kategori dari yang paling banyak terjual dan paling sedikit terjual.
-category_counts = main_df['product_category_name'].value_counts()
+category_counts = filtered_df['product_category_name'].value_counts()
 
 #5 kategori teratas dan terbawah
 top_5 = category_counts.head(5)
